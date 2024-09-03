4171b405995b65c493e95035f8d634c964939277
Tori <7orivorian+github@gmail.com>
2024-09-02 22:42:18 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <>
+            <div className="centered-container-flex">
+                <h1>Heading Here</h1>
+            </div>
+        </>
+    )
+}
+
+export default App
