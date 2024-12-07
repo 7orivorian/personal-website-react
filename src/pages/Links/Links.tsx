725387548084ac4a0b8b79e4b14a750fe4bd8e10
Tori <7orivorian+github@gmail.com>
2024-12-07 00:03:47 -0500

    Refactor Link components for dynamic social link loading
    - Removed hardcoded social links in `Links.tsx` and implemented dynamic fetching with `fetchSocialLinks`, allowing for easier updates and management.
    - Introduced `getIconComponentByName` function to dynamically render icons based on provided link data, increasing modularity.
    - Enhanced responsiveness in `links.scss` by making the container adapt to screen width using media queries.

--- src/pages/Links/Links.tsx
+++ src/pages/Links/Links.tsx
@@ -1,8 +1,7 @@
 import "./links.scss";
+import {handleUrlClick} from "../../scripts/utils.ts";
 import FadeInSection from "../../components/FadeInSection/FadeInSection.tsx";
 import CardSection from "../../components/cards/CardSection/CardSection.tsx";
-import {handleUrlClick} from "../../scripts/utils.ts";
-import {useNavigate} from "react-router-dom";
 import ListCard from "../../components/cards/ListCard/ListCard.tsx";
 import HomeIcon from "../../components/icon/material/HomeIcon.tsx";
 import GitHubIcon from "../../components/icon/GitHubIcon.tsx";
@@ -11,10 +10,12 @@ import BSkyIcon from "../../components/icon/BSkyIcon.tsx";
 import ThreadsIcon from "../../components/icon/ThreadsIcon.tsx";
 import OnlyFansIcon from "../../components/icon/OnlyFansIcon.tsx";
 import MailIcon from "../../components/icon/material/MailIcon.tsx";
+import {fetchSocialLinks, SocialLinkData} from "../../scripts/fetchers.ts";
+import ErrorIcon from "../../components/icon/material/ErrorIcon.tsx";
+
+const resource = fetchSocialLinks();
 
 export default function Links() {
-    const navigate = useNavigate();
-
     return (
         <div className="links-content-container">
             <FadeInSection>
@@ -28,37 +29,47 @@ export default function Links() {
                 </div>
             </FadeInSection>
 
-            <FadeInSection>
-                <CardSection>
-                    <ListCard header={"Website"} text={"Learn more about me."} clickHandler={() => navigate('/')}>
-                        <HomeIcon/>
-                    </ListCard>
-                    <ListCard header={"GitHub"} text={"Browse my personal projects."}
-                              clickHandler={() => handleUrlClick(import.meta.env.VITE_GITHUB_LINK)}>
-                        <GitHubIcon/>
-                    </ListCard>
-                    <ListCard header={"Linkedin"} text={"Connect with me on Linkedin."}
-                              clickHandler={() => handleUrlClick(import.meta.env.VITE_LINKEDIN_LINK)}>
-                        <LinkedinIcon/>
-                    </ListCard>
-                    <ListCard header={"Bluesky"} text={"Follow me on Bluesky."}
-                              clickHandler={() => handleUrlClick(import.meta.env.VITE_BSKY_LINK)}>
-                        <BSkyIcon/>
-                    </ListCard>
-                    <ListCard header={"Threads"} text={"Follow me on threads."}
-                              clickHandler={() => handleUrlClick(import.meta.env.VITE_THREADS_LINK)}>
-                        <ThreadsIcon/>
-                    </ListCard>
-                    <ListCard header={"Email"} text={"Send me an email."}
-                              clickHandler={() => handleUrlClick(import.meta.env.VITE_EMAIL_LINK)}>
-                        <MailIcon/>
-                    </ListCard>
-                    <ListCard header={"OnlyFans"} text={"View exclusive content."}
-                              clickHandler={() => handleUrlClick("https://www.youtube.com/watch?v=dQw4w9WgXcQ")}>
-                        <OnlyFansIcon/>
-                    </ListCard>
-                </CardSection>
-            </FadeInSection>
+            <LinkSection/>
         </div>
     );
+}
+
+function LinkSection() {
+    const data = resource.read();
+
+    return (
+        <FadeInSection>
+            <CardSection>
+                {data.map((item: SocialLinkData) => {
+                    return (
+                        <ListCard key={item.name} header={item.name} text={item.description}
+                                  clickHandler={() => handleUrlClick(item.url)}>
+                            {getIconComponentByName(item.icon)}
+                        </ListCard>
+                    );
+                })}
+            </CardSection>
+        </FadeInSection>
+    );
+}
+
+function getIconComponentByName(name: string) {
+    switch (name.trim().toLowerCase()) {
+        case "home":
+            return <HomeIcon/>;
+        case "github":
+            return <GitHubIcon/>;
+        case "linkedin":
+            return <LinkedinIcon/>;
+        case "bluesky":
+            return <BSkyIcon/>;
+        case "threads":
+            return <ThreadsIcon/>;
+        case "mail":
+            return <MailIcon/>;
+        case "onlyfans":
+            return <OnlyFansIcon/>;
+        default:
+            return <ErrorIcon/>;
+    }
 }
